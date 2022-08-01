64d8418c30bb987c40ac2c0bec60c7655f2f9504
gabefando <gabe.fonseca.04@gmail.com>
2022-07-31 23:17:26 -0400

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -5,7 +5,56 @@ const path = require('path')
 const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "github",
+        message: "Enter your GitHub Username:"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter your Email Address:"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "Enter the name of the project:"
+    },
+    {
+        type: "input",
+        name: "desc",
+        message: "Write a description of the project:"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Choose a license for your project",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Enter the command you will use to install dependencies (default is npm i)",
+        default: "npm i"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Enter the command you will use to run tests (default is npm test)",
+        default: "npm test"
+    },
+    {
+        type: "input",
+        name: "projectusage",
+        message: "Enter anything the user has to know about the project"
+    },
+    {
+        type: "input",
+        name: "contributions",
+        message: "Enter any info the user needs to know about contributing to the project"
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
